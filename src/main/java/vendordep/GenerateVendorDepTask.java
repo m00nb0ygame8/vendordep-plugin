cb229aa9f5bd6d007331d3c5259558ce8bc97ae4
Moon <moonjavadev@gmail.com>
2025-09-06 22:53:48 -0400

    Bumped version and updated GenerateVendorDepTask

--- src/main/java/vendordep/GenerateVendorDepTask.java
+++ src/main/java/vendordep/GenerateVendorDepTask.java
@@ -1,22 +1,19 @@
 package vendordep;
 
+import com.google.gson.*;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URL;
 
 public abstract class GenerateVendorDepTask extends DefaultTask {
     @Input
     public abstract Property<String> getDepUrl();
 
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @TaskAction
     public void generate() {
@@ -26,42 +23,46 @@ public abstract class GenerateVendorDepTask extends DefaultTask {
             return;
         }
 
-        try {
-            parseJson(new URI(url).toURL());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch or parse JSON", e);
-        }
-    }
-
-    public void parseJson(URL url) throws Exception {
-        try (InputStreamReader reader = new InputStreamReader(url.openStream())) {
+        try (InputStreamReader reader = new InputStreamReader(new URL(url).openStream())) {
             JsonObject json = GSON.fromJson(reader, JsonObject.class);
 
+            // Start vendorDep block
+            System.out.println("vendorDep {");
+
+            // Repos
             JsonArray mavenUrls = json.getAsJsonArray("mavenUrls");
-            String mavenRepoUrl = mavenUrls.get(0).getAsString();
+            for (JsonElement elem : mavenUrls) {
+                String repoUrl = elem.getAsString();
+                System.out.println("    mavenRepo(\"" + repoUrl + "\")");
+            }
 
-            System.out.println("mavenRepo(\"" + mavenRepoUrl + "\")");
-
+            // Java deps
             JsonArray javaDeps = json.getAsJsonArray("javaDependencies");
-            for (int i = 0; i < javaDeps.size(); i++) {
-                JsonObject dep = javaDeps.get(i).getAsJsonObject();
+            for (JsonElement elem : javaDeps) {
+                JsonObject dep = elem.getAsJsonObject();
                 String groupId = dep.get("groupId").getAsString();
                 String artifactId = dep.get("artifactId").getAsString();
                 String version = dep.get("version").getAsString();
-
-                System.out.println("vendorDep(\"" + groupId + "\", \"" + artifactId + "\", \"" + version + "\")");
+                System.out.println("    vendorDep(\"" + groupId + "\", \"" + artifactId + "\", \"" + version + "\")");
             }
 
+            // JNI deps
             JsonArray jniDeps = json.getAsJsonArray("jniDependencies");
-            for (int i = 0; i < jniDeps.size(); i++) {
-                JsonObject dep = jniDeps.get(i).getAsJsonObject();
+            for (JsonElement elem : jniDeps) {
+                JsonObject dep = elem.getAsJsonObject();
                 String groupId = dep.get("groupId").getAsString();
                 String artifactId = dep.get("artifactId").getAsString();
                 String version = dep.get("version").getAsString();
                 boolean isJar = dep.get("isJar").getAsBoolean();
-
-                System.out.println("nativeDep(\"" + groupId + "\", \"" + artifactId + "\", \"" + version + "\", wpi.platforms.desktop, " + isJar + ")");
+                // For now just hardcode desktop like your Groovy version
+                System.out.println("    nativeDep(\"" + groupId + "\", \"" + artifactId + "\", \"" + version + "\", wpi.platforms.desktop, " + isJar + ")");
             }
+
+            // End vendorDep block
+            System.out.println("}");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch or parse vendordep JSON", e);
         }
     }
 }
